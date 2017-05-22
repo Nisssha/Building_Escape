b42fc063732a564cbf745d3a795415407a686860
Nisssha <nisha@vp.pl>
2017-05-22 19:54:09 +0200

    First version - nothing in here

---
+++ Source/Section_02/Section_02GameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "Section_02GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SECTION_02_API ASection_02GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
